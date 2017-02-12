4dc20dab6aaa534b84f6a80aef081eb4c1d221e3
anirudh005 <anirudh.v.1992@gmail.com>
2017-02-12 20:39:11 +0530

    added local angular.min.js and jquery

---
+++ AnirudhV/Scripts/AppJs/home.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../angular.min.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+    $scope.message1 = "This Page Is Under Construction ...";
+    $scope.message2 = "Stay Tuned ...";
+});
